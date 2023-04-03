263b8ff7fd1b70946c221cc2c8c50a1f9f29add3
lu058294 <lu058294@intelbras.com.br>
2023-04-03 20:58:14 -0300

    fix: removed simple quote please

--- controladores/hospede.js
+++ controladores/hospede.js
@@ -1,14 +1,14 @@
 const database = require("../database");
 
 exports.cadastrarHospede = (req, res)=>{
-    const query = "INSERT INTO hospede (nome) values ($1)";
+    const query = "INSERT INTO hospede (nome) values ($1);"
     const values = [req.body.nome]
-    database.query(query,values).then(
+    database.query(query, values).then(
         (resultado)=>{
-            res.status(200).send({result: resultado.rows})
+            res.status(200).send({result: "sucesso"})
         },
         (erro)=> {
-            res.status(40).send({erro: erro})
+            res.status(400).send({erro: erro})
         }
     )
 }
