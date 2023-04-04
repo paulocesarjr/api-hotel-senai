bc8d32bd0dc7bd287298e4fedfc8551f203735d5
oliveragabriel <g.olivera1993@gmail.com>
2023-04-04 20:14:52 -0300

    feat: add connection between server and routes

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const port = 3000
 const app = express()
 app.use(bodyParser.json())
 
+const quartoRoutes = require('./rotas/quarto.js')
+app.use("/quarto", quartoRoutes)
+
 app.listen(port, () => {
     console.log('API executando')
 })
