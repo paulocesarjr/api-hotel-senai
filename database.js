fc181d889d1057afc5de3d5ef9763bfd2f522912
EmelySanto <emelysantina00@gmail.com>
2023-04-04 19:23:21 -0300

    feat guest: delet guest

---
+++ database.js
@@ -0,0 +1,14 @@
+
+
+const pg = require('pg')
+const database = new pg.Client('postgres://rxfrhsgn:1n2BoiZQSkbzFF5hhY6r0cvsGsqLqWSh@babar.db.elephantsql.com/rxfrhsgn')
+
+database.connect((erro) => {
+    if (erro) {
+        return console.log('Não foi possível se conectar ao ElephantSQL')
+    } else {
+        return console.log('Conectado ao ElephantSQL')
+    }
+})
+
+module.exports = database
